6952b6231cdbf432a017a749d524a450a1fcb93c
wei.wang <xa_wangwei@shuyun.com>
2017-08-31 20:19:16 +0800

    add

--- src/scope/scope.js
+++ src/scope/scope.js
@@ -1,6 +1,9 @@
 export default class scope {
     constructor() {
         this.$$watchList = [];
+        this.$$asyncQueue = [];
+        this.$$children = [];
+        this.$$listeners = [];
     }
 
     $watch(watchFn, calledFn = () => {
@@ -16,24 +19,36 @@ export default class scope {
         };
     }
 
+    $$everyScope(fn) {
+        if (fn(this)) {
+            return this.$$children.every(child => child.$$everyScope(fn));
+        }
+    }
+
     $digestOnce() {
         let isDirty;
-        this.$$watchList.forEach(watch => {
-            try {
-                let newVal = watch.watchFn();
-                let oldVal = watch.last;
-                if (!Object.is(newVal, oldVal)) {
-                    this.$$lastDirtyVal = watch;
-                    watch.last = newVal;
-                    watch.calledFn(newVal, oldVal === this.initLastVal ? newVal : oldVal);
-                    isDirty = true;
-                } else if (this.$$lastDirtyVal === watch) {
-                    return false;
+        let isloop = true;
+        this.$$everyScope(scope => {
+            scope.$$watchList.forEach(watch => {
+                try {
+                    let newVal = watch.watchFn();
+                    let oldVal = watch.last;
+                    if (!Object.is(newVal, oldVal)) {
+                        this.$$lastDirtyVal = watch;
+                        watch.last = newVal;
+                        watch.calledFn(newVal, oldVal === this.initLastVal ? newVal : oldVal);
+                        isDirty = true;
+                    } else if (this.$$lastDirtyVal === watch) {
+                        isloop = false;
+                        return false;
+                    }
+                } catch (e) {
+                    throw e;
                 }
-            } catch (e) {
-                throw e;
-            }
+                return isloop;
+            });
         });
+
         return isDirty;
     }
 
@@ -42,11 +57,15 @@ export default class scope {
         let count = 10;
         this.$$lastDirtyVal = null;
         do {
-            if (isDirty && !count--) {
+            while (this.$$asyncQueue.length) {
+                const task = this.$$asyncQueue.shift();
+                this.$eval(task.expression);
+            }
+            if ((isDirty || this.$$asyncQueue.length) && !count--) {
                 throw new Error('more than 10 s');
             }
             isDirty = this.$digestOnce();
-        } while (isDirty);
+        } while (isDirty || this.$$asyncQueue.length);
     }
 
     $eval(expr, locals) {
@@ -64,6 +83,48 @@ export default class scope {
 
     }
 
+    $evalAsync(expr) {
+        this.$$asyncQueue.push({scope: this, expression: expr});
+    }
+
+    $new(isolated) {
+        let childScope;
+        if (isolated) {
+            childScope = new scope();
+        } else {
+            const constructor = () => {
+            };
+            constructor.prototype = this;
+            childScope = new constructor();
+        }
+
+        childScope.$$watchList = [];
+        childScope.$$listeners = {};
+        this.$$children.push(childScope);
+        return childScope;
+    }
+
+    $on(eventName, listener) {
+        let listeners = this.$$listeners[eventName];
+        if (!listeners) {
+            this.$$listeners[eventName] = listeners = [];
+        }
+        listeners.push(listener);
+    }
+    $emit(eventName) {
+        this.$$fireEventOnScope();
+    }
+    $broadcast(eventName) {
+        this.$$fireEventOnScope();
+    }
+    $$fireEventOnScope(eventName) {
+        let listeners = this.$$listeners[eventName];
+        let event = {name: eventName}
+        listeners.forEach(item => item(event));
+    }
+
     initLastVal() {
     }
+
+
 }
